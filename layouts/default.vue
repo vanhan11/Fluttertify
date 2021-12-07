ff4dc9a09e8d1a74b1cbd69fcf3345666031fd8e
vanhan200902 <vanhan200902@gmail.com>
2021-12-07 13:05:34 +0700

    delete clear

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,20 +12,3 @@
 
   </v-app>
 </template>
-
-<style>
-.fixed-container{
-    position: fixed;
-    height: 100vh;
-    width: 100vw;
-    z-index: 999;
-    top: 0;
-    left: 0;
-    opacity: 0;
-    pointer-events: none;
-    transition: all .3s;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-</style>
